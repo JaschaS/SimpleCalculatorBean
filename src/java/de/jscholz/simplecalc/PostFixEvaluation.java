ab9b9edd931d793672cf420e17e82dbfa0cb3af8
JaschaS <Jascha@jaschas-mbp.fritz.box>
2017-04-25 21:08:00 +0200

    New Evaluation Tests and post fix evaluation fixes.

--- src/java/de/jscholz/simplecalc/PostFixEvaluation.java
+++ src/java/de/jscholz/simplecalc/PostFixEvaluation.java
@@ -1,6 +1,8 @@
 package de.jscholz.simplecalc;
 
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -8,34 +10,55 @@ import java.util.Stack;
  */
 public class PostFixEvaluation {
 
+    private final Pattern positiveNumberPattern;
+    private final Pattern negativeNumberPattern;
+    private Matcher numberMatcher;
+
+    public PostFixEvaluation () {
+        this.positiveNumberPattern = Pattern.compile ( "[0-9]+" );
+        this.negativeNumberPattern = Pattern.compile ( "um[1-9][0-9]*" );
+        this.numberMatcher = null;
+    }
+
     public String evaluate ( final String expression ) {
 
-        //TODO Expression should be valid
-        //TODO Division is allowed, but integer is been used for calculation.
-        
         final Stack<String> operands = new Stack<> ();
         final String[] seperated = expression.split ( "\\s" );
 
         for ( final String c : seperated ) {
 
-            if ( c.matches ( "[0-9]" ) ) {
+            numberMatcher = positiveNumberPattern.matcher ( c );
+
+            if ( numberMatcher.matches () ) {
                 operands.push ( c );
             }
             else {
 
-                int second = Integer.parseInt ( operands.pop () );
-                int first = Integer.parseInt ( operands.pop () );
-                
-                final int result = calculate ( first, second, c );
-                
-                operands.push ( result + "" );
+                numberMatcher = negativeNumberPattern.matcher ( c );
+
+                if ( numberMatcher.matches () ) {
+
+                    //Remove um in c.
+                    final String s = c.substring ( 2, c.length () );
+
+                    //Add minus symbol to number.
+                    operands.push ( "-" + s );
+
+                }
+                else {
+                    int second = Integer.parseInt ( operands.pop () );
+                    int first = Integer.parseInt ( operands.pop () );
+
+                    final int result = calculate ( first, second, c );
+
+                    operands.push ( result + "" );
+                }
 
             }
 
         }
-        
+
         //TODO the operand should only contain one element at the end. Validate this.
-        
         return operands.pop ();
     }
 
@@ -49,9 +72,6 @@ public class PostFixEvaluation {
 
                 return first - second;
             case "*":
-
-                return first * second;
-            case "/":
 
                 return first * second;
         }
--- test/PostFixEvaluationTest.java
+++ test/PostFixEvaluationTest.java
@@ -10,53 +10,147 @@ import org.junit.Before;
  * @author JScholz
  */
 public class PostFixEvaluationTest {
-    
+
     private PostFixEvaluation evaluation;
     private ShuntingYard algo;
-    
+
     @Before
-    public void setup() {
+    public void setup () {
         evaluation = new PostFixEvaluation ();
         algo = new ShuntingYard ();
     }
-    
+
     @Test
-    public void simpleTest() {
-    
-        final String[] expression = new String[] {"5 5 +","2 3 *", "4 3 -", "3 3 *"};
-        final String[] results = new String[] {"10","6", "1", "9"};
-    
-        for(int i=0; i < expression.length; ++i) {
-        
-            final String result = evaluation.evaluate ( expression[i] );
-            
-            assertTrue ( result != null && !result.isEmpty ());
-            
-            System.out.println ( result + " " + result.length () );
-            
-            assertTrue ( result.equals ( results[i] ) );
-            
-        }
-        
-    }
-    
-    @Test
-    public void complexTest() {
-    
-        final String[] expression = new String[] {"5 + 5 - 3 + 5","2 * 3 + 5 - 3","4 - 3 * 4 * 5 + 1","1 * 3 + 4 * 4"};
-        final String[] results = new String[] {"12", "8", "-55", "19"};
-        
-        for(int i=0; i < expression.length; ++i) {
-        
-            final String postFix = algo.createPostFixExpression ( expression[i] );
-            final String result = evaluation.evaluate ( postFix );
-            
-            assertTrue ( result != null && !result.isEmpty ());
-            
-            System.out.println ( postFix + " r: " + result + " l: " + result.length () );
-            
-            assertTrue ( result.equals ( results[i] ) );
-            
+    public void evaluateSimpleAddTest () {
+        final String[][] expression = new String[][]{
+            { "1 + 3", "1 3 +", "4" },
+            { "5 + 8", "5 8 +", "13" },
+            { "15 + 38", "15 38 +", "53" },
+            { "20 + 0", "20 0 +", "20" },
+            { "113 + 1", "113 1 +", "114" },
+            { "2 + 8", "2 8 +", "10" },
+            { "0 + 3", "0 3 +", "3" },
+            { "0 + 0", "0 0 +", "0" },
+            { "um55 + 28", "um55 28 +", "-27" },
+            { "um17 + 13", "um17 13 +", "-4" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            final String evaluate = evaluation.evaluate ( postFix );
+            assertTrue ( evaluate.equals ( expression[ i ][ 2 ] ) );
         }
     }
+
+    @Test
+    public void evaluateSimpleSubTest () {
+        final String[][] expression = new String[][]{
+            { "1 - 3", "1 3 -", "-2" },
+            { "5 - 8", "5 8 -", "-3" },
+            { "15 - 38", "15 38 -", "-23" },
+            { "20 - 0", "20 0 -", "20" },
+            { "113 - 1", "113 1 -", "112" },
+            { "2 - 8", "2 8 -", "-6" },
+            { "um55 - 28", "um55 28 -", "-83" },
+            { "um17 - 13", "um17 13 -", "-30" },
+            { "0 - 3", "0 3 -", "-3" },
+            { "0 - 0", "0 0 -", "0" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            final String evaluate = evaluation.evaluate ( postFix );
+            assertTrue ( evaluate.equals ( expression[ i ][ 2 ] ) );
+        }
+    }
+
+    @Test
+    public void evaluateSimpleMulTest () {
+        final String[][] expression = new String[][]{
+            { "um3 * 2", "um3 2 *", "-6" },
+            { "4 * 4", "4 4 *", "16" },
+            { "4 * um3", "4 um3 *", "-12" },
+            { "12 * 7", "12 7 *", "84" },
+            { "um28 * um2", "um28 um2 *", "56" },
+            { "5 * 5", "5 5 *", "25" },
+            { "6 * 3", "6 3 *", "18" },
+            { "12 * 12", "12 12 *", "144" },
+            { "2 * 8", "2 8 *", "16" },
+            { "um13 * um3", "um13 um3 *", "39" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            final String evaluate = evaluation.evaluate ( postFix );
+            assertTrue ( evaluate.equals ( expression[ i ][ 2 ] ) );
+        }
+    }
+
+    @Test
+    public void evaluateComplexAddTest () {
+        final String[][] expression = new String[][]{
+            { "132 + 3 + 45 + 5 + 10", "132 3 + 45 + 5 + 10 +", "195" },
+            { "5 + 8 + 34 + 23 + 44", "5 8 + 34 + 23 + 44 +", "114" },
+            { "15 + 38 + 5", "15 38 + 5 +", "58" },
+            { "20 + 0 + 0", "20 0 + 0 +", "20" },
+            { "0 + 0 + 0 + 0", "0 0 + 0 + 0 +", "0" },
+            { "2 + 8 + 34 + 73 + 23", "2 8 + 34 + 73 + 23 +", "140" },
+            { "123 + 34 + 3444 + 34", "123 34 + 3444 + 34 +", "3635" },
+            { "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9", "1 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 +", "45" },
+            { "0 + 3 + 0", "0 3 + 0 +", "3" },
+            { "0 + 0 + 1", "0 0 + 1 +", "1" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            final String evaluate = evaluation.evaluate ( postFix );
+            assertTrue ( evaluate.equals ( expression[ i ][ 2 ] ) );
+        }
+    }
+
+    @Test
+    public void evaluateComplexSubTest () {
+        final String[][] expression = new String[][]{
+            { "um132 - 3 - 45 - 5 - 10", "um132 3 - 45 - 5 - 10 -", "-195" },
+            { "um5 - 8 - 34 - 23 - 44", "um5 8 - 34 - 23 - 44 -", "-114" },
+            { "um15 - 38 - 5", "um15 38 - 5 -", "-58" },
+            { "um20 - 0 - 0", "um20 0 - 0 -", "-20" },
+            { "0 - 0 - 0 - 0", "0 0 - 0 - 0 -", "0" },
+            { "um2 - 8 - 34 - 73 - 23", "um2 8 - 34 - 73 - 23 -", "-140" },
+            { "123 - 34 - 3444 - 34", "123 34 - 3444 - 34 -", "-3389" },
+            { "1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9", "1 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 -", "-43" },
+            { "0 - 3 - 0", "0 3 - 0 -", "-3" },
+            { "0 - 0 - 1", "0 0 - 1 -", "-1" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            final String evaluate = evaluation.evaluate ( postFix );
+            assertTrue ( evaluate.equals ( expression[ i ][ 2 ] ) );
+        }
+    }
+
+    @Test
+    public void evaluateComplexMixTest () {
+        final String[][] expression = new String[][]{
+            { "um132 * 3 + 45 + 5 * 10", "um132 3 * 45 + 5 10 * +", "-301" },
+            { "5 * 8 + 3", "5 8 * 3 +", "43" },
+            { "um15 - 38 * 5", "um15 38 5 * -", "-205" },
+            { "um20 * 0 - 0", "um20 0 * 0 -", "0" },
+            { "0 + 0 * 0 - 0", "0 0 0 * + 0 -", "0" },
+            { "um2 + 8 - 34 - 73 * 23", "um2 8 + 34 - 73 23 * -", "-1707" },
+            { "123 - 34 + 3444 + 34", "123 34 - 3444 + 34 +", "3567" },
+            { "1 - 2 - 3 * 4 + 5 + 6 - 7 - 8 - 9", "1 2 - 3 4 * - 5 + 6 + 7 - 8 - 9 -", "-26" },
+            { "0 * 3 - 0", "0 3 * 0 -", "0" },
+            { "0 + 0 - 1", "0 0 + 1 -", "-1" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            final String evaluate = evaluation.evaluate ( postFix );
+            System.out.println ( "postfix: " + postFix + " ev: " + evaluate + " exp: " + expression[ i ][ 2 ] );
+            assertTrue ( evaluate.equals ( expression[ i ][ 2 ] ) );
+        }
+    }
+
 }
