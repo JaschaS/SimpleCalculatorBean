318fe399991c9248eb1ab82c55de28fd1e5a584a
JaschaS <Jascha@jaschas-mbp.fritz.box>
2017-04-25 17:44:05 +0200

    Removed unused code in Test.

--- test/ShuntingYardTest.java
+++ test/ShuntingYardTest.java
@@ -200,40 +200,5 @@ public class ShuntingYardTest {
             assertTrue ( postFix.equals ( expression[ i ][ 1 ] ) );
         }
     }
-    /*
-     * @Test public void simpleTest () {
-     *
-     * final String[] expression = new String[]{ "5 + 5", "2 * 3", "4 - 3", "1 / 3" }; final String[] results = new String[]{ "5 5 +", "2 3 *", "4 3 -", "1 3 /" };
-     *
-     * for ( int i = 0; i < expression.length; ++i ) {
-     *
-     * final String postFix = algo.createPostFixExpression ( expression[ i ] );
-     *
-     * assertTrue ( postFix != null && !postFix.isEmpty () );
-     *
-     * System.out.println ( postFix + " " + postFix.length () );
-     *
-     * assertTrue ( postFix.equals ( results[ i ] ) );
-     *
-     * }
-     * }
-     *
-     * @Test public void complexTest () {
-     *
-     * final String[] expression = new String[]{ "5 + 5 - 3 + 5", "2 * 3 + 5 - 3", "4 - 3 * 4 * 5 + 1", "1 / 3 + 4 * 4" }; final String[] results = new String[]{ "5 5 + 3 - 5 +",
-     * "2 3 * 5 + 3 -", "4 3 4 * 5 * - 1 +", "1 3 / 4 4 * +" };
-     *
-     * for ( int i = 0; i < expression.length; ++i ) {
-     *
-     * final String postFix = algo.createPostFixExpression ( expression[ i ] );
-     *
-     * assertTrue ( postFix != null && !postFix.isEmpty () );
-     *
-     * System.out.println ( postFix + " " + postFix.length () );
-     *
-     * assertTrue ( postFix.equals ( results[ i ] ) );
-     *
-     * }
-     * }
-     */
+
 }
