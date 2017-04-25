3f4f6329ed480e148b0e174c400f67b3f5114353
JaschaS <Jascha@jaschas-mbp.fritz.box>
2017-04-25 17:13:30 +0200

    New Shunting Yard Tests and Regex expressions.

--- src/java/de/jscholz/simplecalc/ShuntingYard.java
+++ src/java/de/jscholz/simplecalc/ShuntingYard.java
@@ -2,6 +2,8 @@
 package de.jscholz.simplecalc;
 
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A simple Post Fix Expression generator.
@@ -10,31 +12,66 @@ import java.util.Stack;
  */
 public class ShuntingYard {
 
-    public String createPostFixExpression (final String expression ) {
+    private final Pattern numberRegexPattern;
+    private Matcher numberRegexMatcher;
+    
+    public ShuntingYard() {
         
-        /*
-         * We expect that the given expression is valid.
-         * The number is less than 10. Which means a single character.
-         * Numbers and Operators are seperated by whitespaces.
+        /* To detect if the substring is a number or an operator,
+         * the following regex is been used.
+         * 
+         * [0-9]+ => checks if the number is only positive
+         * 
+         * um[1-9][0-9]* => checks a negative number.
+         * um defines the unary minus.
+         * After the unary minus follows a number [1-9]. By definition
+         * zero can't be negative.
+         * [0-9]* is for more digits.
+         * 
+         * Note: the unary minus is only needed at the beginning of a
+         * expression.
          */
-        //TODO check if expression is valid and not null.
+        numberRegexPattern = Pattern.compile ( "[0-9]+|um[1-9][0-9]*" );
+        numberRegexMatcher = null;
+    }
+    
+    public Pattern getNumberRegex() {
+        return numberRegexPattern;
+    }
+    
+    /**
+     * Generates a post fix expression from the given expression.
+     * 
+     * there will be no validation check. Do a validation check before
+     * calling the method.
+     * 
+     * @param expression a expression in infix form.
+     * @return 
+     */
+    public String createPostFixExpression (final String expression ) {
         
         final Stack<String> operators = new Stack<>();
         final StringBuilder output = new StringBuilder ();
         
-        final String[] seperated = expression.split ( "\\s" );
+        final String[] seperated = expression.split ( "\\s+" );
         
         for(final String c : seperated) {
     
+            if(numberRegexMatcher == null) numberRegexMatcher = numberRegexPattern.matcher ( c );
+            else numberRegexMatcher.reset ( c );
+            
             //If it is a number, add it to the output.
-            if(c.matches ( "[0-9]")) {
+            //The number contains only digits or um and digits,
+            //where um is unary minus.
+            if(numberRegexMatcher.matches ()) {
+                //Add a whitespace to the output to separate the operators and numbers.
                 output.append ( c ).append ( " " );
             }
             //Otherwise it is a operator. Again, we expect that this expression is valid.
             else {
             
                     //Check if the new operator has a higher order.
-                    // Div >= mul > add >= sub
+                    //mul > add >= sub
                     final int size = operators.size ();
                     final int currentOrder = getOrder ( c );
                     int i=0;
@@ -73,7 +110,6 @@ public class ShuntingYard {
         if(operator.equals ( "+" )) return 1;
         if(operator.equals ( "-" )) return 1;
         if(operator.equals ( "*" )) return 2;
-        if(operator.equals ( "/" )) return 2;
         
         return 0;
     }
--- test/ShuntingYardTest.java
+++ test/ShuntingYardTest.java
@@ -1,5 +1,7 @@
 
 import de.jscholz.simplecalc.ShuntingYard;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,6 +14,60 @@ public class ShuntingYardTest {
     private final ShuntingYard algo = new ShuntingYard ();
 
     @Test
+    public void validPositiveNumbersRegexTest () {
+
+        final Pattern regexPattern = algo.getNumberRegex ();
+        final Matcher match = regexPattern.matcher ( "" );
+        final String[] numbers = new String[]{
+            "3",
+            "13",
+            "1293",
+            "0",
+            "25",
+            "34",
+            "2",
+            "1",
+            "10",
+            "100",
+            "77"
+        };
+
+        for ( int i = 0; i < numbers.length; ++i ) {
+
+            match.reset ( numbers[ i ] );
+            assertTrue ( match.matches () );
+
+        }
+    }
+
+    @Test
+    public void validNegativeNumbersRegexTest () {
+
+        final Pattern regexPattern = algo.getNumberRegex ();
+        final Matcher match = regexPattern.matcher ( "" );
+        final String[] numbers = new String[]{
+            "um3",
+            "um13",
+            "um1293",
+            "um22",
+            "um25",
+            "um34",
+            "um2",
+            "um1",
+            "um10",
+            "um100",
+            "um77"
+        };
+
+        for ( int i = 0; i < numbers.length; ++i ) {
+
+            match.reset ( numbers[ i ] );
+            assertTrue ( match.matches () );
+
+        }
+    }
+
+    @Test
     public void simpleAddTest () {
         final String[][] expression = new String[][]{
             { "1 + 3", "1 3 +" },
@@ -20,8 +76,8 @@ public class ShuntingYardTest {
             { "20 + 0", "20 0 +" },
             { "113 + 1", "113 1 +" },
             { "2 + 8", "2 8 +" },
-            { "bn55 + 28", "bn55 28 +" },
-            { "bn17 + 13", "bn17 13 +" },
+            { "um55 + 28", "um55 28 +" },
+            { "um17 + 13", "um17 13 +" },
             { "0 + 3", "0 3 +" },
             { "0 + 0", "0 0 +" }
         };
@@ -41,8 +97,8 @@ public class ShuntingYardTest {
             { "20 - 0", "20 0 -" },
             { "113 - 1", "113 1 -" },
             { "2 - 8", "2 8 -" },
-            { "bn55 - 28", "bn55 28 -" },
-            { "bn17 - 13", "bn17 13 -" },
+            { "um55 - 28", "um55 28 -" },
+            { "um17 - 13", "um17 13 -" },
             { "0 - 3", "0 3 -" },
             { "0 - 0", "0 0 -" }
         };
@@ -56,16 +112,16 @@ public class ShuntingYardTest {
     @Test
     public void simpleMulTest () {
         final String[][] expression = new String[][]{
-            { "bn3 * 2", "bn3 2 *" },
+            { "um3 * 2", "um3 2 *" },
             { "4 * 4", "4 4 *" },
-            { "4 * bn3", "4 bn3 *" },
+            { "4 * um3", "4 um3 *" },
             { "12 * 7", "12 7 *" },
-            { "bn28 * bn2", "bn28 bn2 *" },
+            { "um28 * um2", "um28 um2 *" },
             { "5 * 5", "5 5 *" },
             { "6 * 3", "6 3 *" },
             { "12 * 12", "12 12 *" },
             { "2 * 8", "2 8 *" },
-            { "bn13 * bn3", "bn13 bn3 *" }
+            { "um13 * um3", "um13 um3 *" }
         };
 
         for ( int i = 0; i < expression.length; ++i ) {
@@ -73,43 +129,103 @@ public class ShuntingYardTest {
             assertTrue ( postFix.equals ( expression[ i ][ 1 ] ) );
         }
     }
-/*
-    @Test
-    public void simpleTest () {
-
-        final String[] expression = new String[]{ "5 + 5", "2 * 3", "4 - 3", "1 / 3" };
-        final String[] results = new String[]{ "5 5 +", "2 3 *", "4 3 -", "1 3 /" };
-
-        for ( int i = 0; i < expression.length; ++i ) {
-
-            final String postFix = algo.createPostFixExpression ( expression[ i ] );
-
-            assertTrue ( postFix != null && !postFix.isEmpty () );
-
-            System.out.println ( postFix + " " + postFix.length () );
-
-            assertTrue ( postFix.equals ( results[ i ] ) );
-
-        }
-    }
 
     @Test
-    public void complexTest () {
-
-        final String[] expression = new String[]{ "5 + 5 - 3 + 5", "2 * 3 + 5 - 3", "4 - 3 * 4 * 5 + 1", "1 / 3 + 4 * 4" };
-        final String[] results = new String[]{ "5 5 + 3 - 5 +", "2 3 * 5 + 3 -", "4 3 4 * 5 * - 1 +", "1 3 / 4 4 * +" };
+    public void complexAddTest () {
+        final String[][] expression = new String[][]{
+            { "132 + 3 + 45 + 5 + 10", "132 3 + 45 + 5 + 10 +" },
+            { "5 + 8 + 34 + 23 + 44", "5 8 + 34 + 23 + 44 +" },
+            { "15 + 38 + 5", "15 38 + 5 +" },
+            { "20 + 0 + 0", "20 0 + 0 +" },
+            { "0 + 0 + 0 + 0", "0 0 + 0 + 0 +" },
+            { "2 + 8 + 34 + 73 + 23", "2 8 + 34 + 73 + 23 +" },
+            { "123 + 34 + 3444 + 34", "123 34 + 3444 + 34 +" },
+            { "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9", "1 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 +" },
+            { "0 + 3 + 0", "0 3 + 0 +" },
+            { "0 + 0 + 1", "0 0 + 1 +" }
+        };
 
         for ( int i = 0; i < expression.length; ++i ) {
-
-            final String postFix = algo.createPostFixExpression ( expression[ i ] );
-
-            assertTrue ( postFix != null && !postFix.isEmpty () );
-
-            System.out.println ( postFix + " " + postFix.length () );
-
-            assertTrue ( postFix.equals ( results[ i ] ) );
-
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            assertTrue ( postFix.equals ( expression[ i ][ 1 ] ) );
         }
     }
-    */
+    
+    @Test
+    public void complexSubTest () {
+        final String[][] expression = new String[][]{
+            { "um132 - 3 - 45 - 5 - 10", "um132 3 - 45 - 5 - 10 -" },
+            { "um5 - 8 - 34 - 23 - 44", "um5 8 - 34 - 23 - 44 -" },
+            { "um15 - 38 - 5", "um15 38 - 5 -" },
+            { "um20 - 0 - 0", "um20 0 - 0 -" },
+            { "0 - 0 - 0 - 0", "0 0 - 0 - 0 -" },
+            { "um2 - 8 - 34 - 73 - 23", "um2 8 - 34 - 73 - 23 -" },
+            { "123 - 34 - 3444 - 34", "123 34 - 3444 - 34 -" },
+            { "1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9", "1 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 -" },
+            { "0 - 3 - 0", "0 3 - 0 -" },
+            { "0 - 0 - 1", "0 0 - 1 -" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            assertTrue ( postFix.equals ( expression[ i ][ 1 ] ) );
+        }
+    }
+    
+    @Test
+    public void complexMixTest () {
+        final String[][] expression = new String[][]{
+            { "um132 * 3 + 45 + 5 * 10", "um132 3 * 45 + 5 10 * +" },
+            { "5 * 8 + 3", "5 8 * 3 +" },
+            { "um15 - 38 * 5", "um15 38 5 * -" },
+            { "um20 * 0 - 0", "um20 0 * 0 -" },
+            { "0 + 0 * 0 - 0", "0 0 0 * + 0 -" },
+            { "um2 + 8 - 34 - 73 * 23", "um2 8 + 34 - 73 23 * -" },
+            { "123 - 34 + 3444 + 34", "123 34 - 3444 + 34 +" },
+            { "1 - 2 - 3 * 4 + 5 + 6 - 7 - 8 - 9", "1 2 - 3 4 * - 5 + 6 + 7 - 8 - 9 -" },
+            { "0 * 3 - 0", "0 3 * 0 -" },
+            { "0 + 0 - 1", "0 0 + 1 -" }
+        };
+
+        for ( int i = 0; i < expression.length; ++i ) {
+            final String postFix = algo.createPostFixExpression ( expression[ i ][ 0 ] );
+            assertTrue ( postFix.equals ( expression[ i ][ 1 ] ) );
+        }
+    }
+    /*
+     * @Test public void simpleTest () {
+     *
+     * final String[] expression = new String[]{ "5 + 5", "2 * 3", "4 - 3", "1 / 3" }; final String[] results = new String[]{ "5 5 +", "2 3 *", "4 3 -", "1 3 /" };
+     *
+     * for ( int i = 0; i < expression.length; ++i ) {
+     *
+     * final String postFix = algo.createPostFixExpression ( expression[ i ] );
+     *
+     * assertTrue ( postFix != null && !postFix.isEmpty () );
+     *
+     * System.out.println ( postFix + " " + postFix.length () );
+     *
+     * assertTrue ( postFix.equals ( results[ i ] ) );
+     *
+     * }
+     * }
+     *
+     * @Test public void complexTest () {
+     *
+     * final String[] expression = new String[]{ "5 + 5 - 3 + 5", "2 * 3 + 5 - 3", "4 - 3 * 4 * 5 + 1", "1 / 3 + 4 * 4" }; final String[] results = new String[]{ "5 5 + 3 - 5 +",
+     * "2 3 * 5 + 3 -", "4 3 4 * 5 * - 1 +", "1 3 / 4 4 * +" };
+     *
+     * for ( int i = 0; i < expression.length; ++i ) {
+     *
+     * final String postFix = algo.createPostFixExpression ( expression[ i ] );
+     *
+     * assertTrue ( postFix != null && !postFix.isEmpty () );
+     *
+     * System.out.println ( postFix + " " + postFix.length () );
+     *
+     * assertTrue ( postFix.equals ( results[ i ] ) );
+     *
+     * }
+     * }
+     */
 }
